5779624bb370ffa94db25fff3e3445025e38df2c
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2024-01-08 18:29:20 +0530

    Add AST Consulting details

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -30,10 +30,11 @@ const Experience = () => {
       <SectionTitle title="Experiences" />
       <div className="w-full mt-10 flex flex-col md:flex-row gap-16 bg-[#112240] rounded-md p-10">
         <ul>
-          {experienceLi("Gail Training Institute", 1)}
-          {experienceLi("Microsoft Engage 2022", 2)}
+          {experienceLi("AST Consulting", 1)}
+          {experienceLi("Gail Training Institute", 2)}
+          {experienceLi("Microsoft Engage 2022", 3)}
         </ul>
-        {activeItem === 1 && (
+        {activeItem === 2 && (
           <Work
             Name="Gail Training Institute"
             Type="Summer Internship | Full Stack Developer"
@@ -47,7 +48,7 @@ const Experience = () => {
             ]}
           />
         )}
-        {activeItem === 2 && (
+        {activeItem === 3 && (
           <Work
             Name="Microsoft"
             Type="Mentee at Engage'22"
@@ -60,6 +61,26 @@ const Experience = () => {
             ]}
           />
         )}
+         {activeItem === 1 && (
+          <Work
+            Name="AST Consulting"
+            Type="Software Devloper Engineer Intern"
+            Duration="3 Months"
+            Points={[
+             "Spearheaded the development of a scalable backend infrastructure using NestJS, MongoDB, and Vite, resulting in a cleaner typescript codes, because of NestJS module micro-architecture.",
+             "Orchestrated the integration of AWS services, optimizing image storage and retrieval, which led to a 50% improvement in image loading speed and a 20% reduction in overall data transfer latency.",
+             "Designed and executed dynamic schedulers that adapt to timezones",
+             "Developed a Telegram bot, enhancing user interaction and experience automating key processes, resulting in a 25% usage by users.",
+             "Integrated social media accounts, consolidating analytics data and providing insights that contributed to a 10% increase in user acquisition and a 15% growth in social media engagement.",
+             "Utilized OpenAI and attempted to integrate in/-house AI models for content generation",
+             "Integrated Chargebee and Stripe payment gateways to streamline payment processing and enhance the platform’s financial capabilities.",
+             "Established an analytics framework by connecting Google Analytics and Microsoft Clarity, leading to actionable insights that improved user journey and resulted in a 30% increase in conversion rates.",
+             "Set up CI/CD pipelines on AWS, reducing deployment time by 40% and ensuring 95%+ uptime of the platform.",
+             "Introduced a comprehensive code review process on GitHub, resulting in a 30% decrease in post-release defects and 20% faster feature delivery.age with designated endpoints for receiving and transmitting requests for getting recommended movies and submitting movie ratings"
+            ]}
+          />
+        )}
+
       </div>
     </section>
   );
